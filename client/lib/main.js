5cc7b904af2883759aa74d34d880990858a500c8
Jason Frame <jason@onehackoranother.com>
2016-04-02 10:54:42 +0100

    basic echo server + line protocol

--- client/lib/main.js
+++ client/lib/main.js
@@ -1,3 +1,47 @@
 window.init = function() {
-    
+	const input = document.querySelector('.input');
+	const output = document.querySelector('.output');
+
+	input.onkeydown = (evt) => {
+		if (evt.which === 13) {
+			const cmd = input.value.trim();
+			if (cmd.length) {
+				socket.send(cmd + "\n");
+			}
+			input.value = "";
+		}
+	};
+
+	const reader = new LineReader();
+	reader.online = (line) => {
+		const div = document.createElement('div');
+		div.textContent = line;
+		output.appendChild(div);
+	}
+
+	const socket = new WebSocket("ws://localhost:8080/");
+	socket.onopen = () => {
+		console.log("socket opened");
+	}
+	socket.onmessage = (msg) => {
+		reader.append(msg.data);
+	}
+}
+
+function LineReader() {
+	var buffer = '';
+	const obj = {
+		append: function(str) {
+			buffer += str;
+			const ix = buffer.indexOf("\n");
+			if (ix >= 0) {
+				const message = buffer.substr(0, ix);
+				buffer = buffer.substr(ix + 1);
+				obj.online(message);
+			}
+			
+		},
+		online: function() {}
+	};
+	return obj;
 }
--- index.js
+++ index.js
@@ -1,11 +1,44 @@
 "use strict";
 
+const EventEmitter = require('events').EventEmitter;
+const WebSocketServer = require('ws').Server
+const byline = require('byline');
+const net = require('net');
+const logger = console;
+
 const PORT = 1234;
 const HOST = '127.0.0.1';
 
-const EventEmitter = require('events').EventEmitter;
-const net = require('net');
-const logger = console;
+//
+//
+
+const wss = new WebSocketServer({ port: 8080 });
+
+wss.on('connection', (ws) => {
+    logger.log("new websocket connection");
+    ws.on('close', close);
+    ws.on('error', (err) => {
+        logger.error("websocket error: %s", err.message);
+        close();
+    });
+    const sock = net.connect(PORT, HOST, () => {
+        ws.on('message', (msg) => { sock.write(msg); });
+        sock.on('data', (data, flags) => { ws.send(data); });
+    });
+    sock.setEncoding('utf8');
+    sock.on('close', close);
+    sock.on('error', (err) => {
+        logger.error("socket error: %s", err.message);
+        close();
+    });
+    function close() {
+        ws.close();
+        sock.end();
+    }
+});
+
+//
+//
 
 const server = net.createServer((socket) => {
     const conn = createConnection(socket);
@@ -18,7 +51,7 @@ server.listen({ port: PORT, host: HOST, }, () => {
     console.log("server listening on %s:%d", HOST, PORT);
 });
 
-let connections = {};
+const connections = {};
 let nextConnectionId = 1;
 function createConnection(socket) {
     const id = nextConnectionId++;
@@ -29,6 +62,16 @@ function createConnection(socket) {
         logger.log("connection id %d closed", conn.id);
         delete connections[id];
     });
+
+    socket.setEncoding('utf8');
+
+    const stream = byline(socket);
+    stream.on('data', (line) => {
+        console.log("cid=%d received line '%s'", id, line);
+        // TODO: parse incoming data + dispatch message
+        socket.write(line + "\n");
+    });
+
     connections[id] = conn;
     logger.log("new connection id %d", conn.id);
     return conn;
